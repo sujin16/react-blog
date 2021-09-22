54da9c58435757b99e7a576f7a43a3bdbf0fa5ef
sujin16 <tnwls9712@ajou.ac.kr>
2021-09-22 15:19:28 +0900

    sdf

--- src/test.js
+++ src/test.js
@@ -3,36 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let[modal,modal_func] =useState(false);
-  let[bname,b_func] =useState('OPEN LIST');
-  
-  function 변경(){
-    modal_func(!modal);
-    if(modal ===true) b_func('OPEN LIST');
-    else b_func('CLOSE LIST');        
-  }
-  return (
-    <div className="App">
-      <Nav/>
-      {
-      modal === true
-      ? <Modal />
-      : null
-	    }
-      <button onClick ={()=>{변경()}}>{bname}</button>
-    </div>
-  );
-}
-
-function Nav(){
-  return(
-    <div className="black-nav">
-      <h2>개발 BLOG</h2>
-    </div>
-  )
-}
-
-function Modal(){
   let [제목,제목바꾸기] = useState(['Java','Python','React']);
   let [날짜,날짜바꾸기] = useState(['2021.08.01','2021.08.02','2021.08.03']);
   let [따봉,따봉변경] =useState([0,0,0]);
@@ -42,23 +12,26 @@ function Modal(){
     nGood[n] = nGood[n]+1;
     따봉변경(nGood);
   }
-  
-  return(
-    <div>
+  return (
+    <div className="App">
+      <div className ="black-nav">
+        <h2>개발 BLOG</h2>
+      </div>
+      <button onClick={()=>{날짜바꾸기}}></button>
       <div className ="list">
         <h2>{제목[0]} <span onClick={()=>{변경(0)}}>🤞</span>{따봉[0]}</h2>
         <p>{날짜[0]}</p><hr/>
       </div>
       <div className ="list">
-        <h2>{제목[1]} <span onClick={()=>{변경(1)}}>🤞</span>{따봉[1]}</h2>
+        <h2>{제목[1]}  <span onClick={()=>{변경(1)}}>🤞</span>{따봉[1]}</h2>
         <p>{날짜[1]}</p><hr/>
       </div>
       <div className ="list">
-        <h2>{제목[2]} <span onClick={()=>{변경(2)}}>🤞</span>{따봉[2]}</h2>
+        <h2>{제목[2]}  <span onClick={()=>{변경(2)}}>🤞</span>{따봉[2]}</h2>
         <p>{날짜[2]}</p><hr/>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
